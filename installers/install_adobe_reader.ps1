d507c589668a2bcb27452a9de0e77c6d3878bfc5
Roxasnake <baptiste.toleb@gmail.com>
2025-08-20 10:12:54 +0200

    Delete installers/install_adobe_reader.ps1

--- installers/install_adobe_reader.ps1
+++
@@ -1,8 +0,0 @@
-# Installe Adobe Acrobat Reader DC
-$adobeUrl = "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/2300820442/AcroRdrDC2300820442_fr_FR.exe"
-$installer = "$env:TEMP\adobe_reader.exe"
-
-Invoke-WebRequest -Uri $adobeUrl -OutFile $installer
-Start-Process $installer -ArgumentList "/sAll /rs /rps /msi /norestart /quiet" -Wait
-Remove-Item $installer -Force
-Write-Host "Adobe Reader installé."
